e949d685accb2edd26e89df005c0f5f70028022c
Ulrich Cuber <git@uc-it.de>
2026-05-06 14:10:23 +0200

    Aufgabe und Daten

---
+++ tag1/aufgaben/bmi_calc.py
@@ -0,0 +1,17 @@
+# BMI Berechnung:
+
+# Eingabe: Name, Grösse, Gewicht
+# Verarbeitung: Daten vorbereiten (Typ!!!) - Berechnung -> Zahl
+# Ausgabe: Eineziel mit "Ist ok: True/False"
+
+# E
+name = input("Name: ")
+groesse = input("Grösse in m: ")
+gewicht = input("Gewicht in kg: ")
+
+# V
+
+
+
+# A
+print(f"......")
