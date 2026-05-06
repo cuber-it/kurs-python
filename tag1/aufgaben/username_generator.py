a80ffba820a16eb5ab9fb13d258f7f1912fbeae8
Ulrich Cuber <git@uc-it.de>
2026-05-06 16:09:53 +0200

    Weitere Beispiele

--- tag1/aufgaben/username_generator.py
+++ tag1/aufgaben/username_generator.py
@@ -4,3 +4,15 @@
 #  kleingeschriebenen Username der Form vorname.nachname.jahr bauen.
 #  Eingabe: Anna / Schmidt / 1995
 #  Ausgabe: anna.schmidt.1995
+
+name = input("Name: ")
+vorname = input("Vorname: ")
+gebjahr = input("Geburtsjahr: ")
+
+username = f"{vorname}.{name}.{gebjahr}"
+
+print("Username: ", username)
+
+username = input("Vorname / Nachname / Geburtsjahr").replace(" ", "").replace("/", ".")
+
+print("Username: ", username)
