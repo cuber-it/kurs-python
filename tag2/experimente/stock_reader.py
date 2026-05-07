1bd87f781816584101637716791f20a1ed843191
Ulrich Cuber <git@uc-it.de>
2026-05-07 15:06:24 +0200

    Tag 2 Experiment mit file

--- tag2/experimente/stock_reader.py
+++ tag2/experimente/stock_reader.py
@@ -2,6 +2,8 @@ path = r"/home/ucuber/Workspace/kurse/kurs-python-gl/materialien/HistoricalQuote
 
 with open(path) as fd:
     quotes = fd.read()
+
+
 print(quotes)
 print(type(quotes))
 print(len(quotes))
@@ -10,13 +12,14 @@ rows = quotes.splitlines() # Liste ohne \n
 print(type(rows))
 print(len(rows))
 
-for row in rows:
-    row = row.split(", ")
-    for field in row:
-        field = field.replace("$", "")
-        try:
-            field = float(field)
-        except ValueError:
-            pass# Pfui! Frickelei!!! :-)
-        print(field, end=" ")
-    print("")
+with open("converted.data", "w") as fd:
+    for row in rows:
+        row = row.split(", ")
+        for field in row:
+            field = field.replace("$", "")
+            try:
+                field = float(field)
+            except ValueError:
+                pass# Pfui! Frickelei!!! :-)
+            print(field, file=fd, end=" ")
+        print("", file=fd)
